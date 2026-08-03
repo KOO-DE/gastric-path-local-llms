e73cbb5fd787bce1e94caf2953f50f47325c2bcb
Kxde <108650600+KOO-DE@users.noreply.github.com>
2026-08-03 21:46:09 +0900

    Lauren Type

--- rule-based/lauren_type.sql
+++ rule-based/lauren_type.sql
@@ -117,7 +117,7 @@ FROM(
                         SUBSTR(병리진단, INSTR(병리진단, 'Lauren classification')) AS Lauren_Classification,
                         SUBSTR(병리진단, INSTR(병리진단, 'Early gastric cancer')) AS Early_Gastric_Cancer
                     FROM(
-                        SELECT * FROM pathologic_biopsy_03
+                        SELECT * FROM pathology_report
                     ) biopsy
                 ) biopsy
             ) biopsy
